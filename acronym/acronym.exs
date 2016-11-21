b11e30314a370e070608f16316c89edff6b3959a
Liz Vanderkloot <liz@lizvdk.com>
2016-11-20 19:47:35 -0500

    it produces acronyms from title case

---
+++ acronym/acronym.exs
@@ -0,0 +1,12 @@
+defmodule Acronym do
+  @doc """
+  Generate an acronym from a string.
+  "This is a string" => "TIAS"
+  """
+  @spec abbreviate(String.t()) :: String.t()
+  def abbreviate(string) do
+    String.split(string)
+    |> Enum.map(fn(word) -> String.first(word) end)
+    |> List.to_string
+  end
+end
