f0fd22a1ddb83c0ee3cb776f6dd00035b4bf0190
Liz Vanderkloot <lizvdk@users.noreply.github.com>
2016-12-11 14:39:37 -0500

    Improve Bob (#4)

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,10 +1,30 @@
 defmodule Bob do
   def hey(input) do
     cond do
-        String.length(String.trim(input)) == 0 -> "Fine. Be that way!"
-        String.last(input) == "?" -> "Sure."
-        String.match?(input, ~r/[^0-9,\W]/) and String.upcase(input) == input -> "Whoa, chill out!"
+        empty?(input) -> "Fine. Be that way!"
+        question?(input) -> "Sure."
+        yelling?(input) -> "Whoa, chill out!"
         true -> "Whatever."
     end
+  end
+
+  defp empty?(input) do
+    String.length(String.trim(input)) == 0
+  end
+
+  defp question?(input) do
+    String.ends_with?(input, "?")
+  end
+
+  defp yelling?(input) do
+    all_caps?(input) and contains_letters?(input)
+  end
+
+  defp all_caps?(input) do
+    String.upcase(input) == input
+  end
+
+  defp contains_letters?(input) do
+    String.match?(input, ~r/[^0-9,\W]/)
   end
 end
